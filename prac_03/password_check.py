006ccd8f6db8337b8a3dfeaafb55c7b4c24b8c97
alexjohnstone29 <alexander.johnstone@my.jcu.edu.au>
2020-08-27 19:57:04 +1000

    Refactor password_check.py to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,23 @@
 MAX_LENGTH = 12
 MIN_LENGTH = 3
 
-password = input("What is your password? ")
-while len(password) > MAX_LENGTH or len(password) < MIN_LENGTH:
-    print("Invalid password")
+
+def main():
+    password = get_password()
+    turn_asterisks(password)
+
+
+def turn_asterisks(password):
+    star_number = len(password)
+    print(star_number * "*")
+
+
+def get_password():
     password = input("What is your password? ")
-star_numb = len(password)
-print(star_numb * "*")
+    while len(password) > MAX_LENGTH or len(password) < MIN_LENGTH:
+        print("Invalid password")
+        password = input("What is your password? ")
+    return password
+
+
+main()
