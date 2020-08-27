f0951fce45ff4d638f984cddd2772de4925b2ca0
alexjohnstone29 <alexander.johnstone@my.jcu.edu.au>
2020-08-27 20:40:36 +1000

    change score check to a function and add random number generator

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,17 +3,28 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
+from random import randint
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-elif score <= 100:
-    print("Excellent")
-else:
-    print("Invalid score")
 
+def main():
+    score = float(input("Enter score: "))
+    print(score_check(score))
+    random_score = randint(1, 100)
+    # print(random_score) # check what the number is
+    print(score_check(random_score))
+
+
+def score_check(score):
+    if score < 0:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    elif score <= 100:
+        return "Excellent"
+    else:
+        return "Invalid score"
+
+
+main()
