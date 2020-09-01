369c644dcdbc15b58da76bce65ece14b91c2e417
Alexander <alexander.johnstone@my.jcu.edu.au>
2020-09-01 18:06:20 +1000

    complete basic list operations

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,4 +3,9 @@ for i in range(5):
     user_number = int(input("Number: "))
     numbers.append(user_number)
 
-print(numbers)
+print(numbers[0])
+print(numbers[-1])
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers) / len(numbers))
+
