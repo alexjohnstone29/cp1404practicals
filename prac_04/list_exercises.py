198ed648bd98307f8d3f6c5466f86fc9b4fdf67f
Alexander <alexander.johnstone@my.jcu.edu.au>
2020-09-01 18:10:44 +1000

    complete Woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,10 @@ print(min(numbers))
 print(max(numbers))
 print(sum(numbers) / len(numbers))
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name = input("What is your username? ")
+if name in usernames:
+    print("Access granted")
+else:
+    print("Access Denied")
+
