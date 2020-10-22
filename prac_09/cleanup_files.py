41cf972da4657f26b1b41b5100f4ba3b07279dab
Alexander <alexander.johnstone@my.jcu.edu.au>
2020-10-23 02:03:24 +1000

    complete cleanup_files.py

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -15,12 +15,22 @@ def main():
         for name in filenames:
             old_name = os.path.join(directory_name, name)
             new_name = os.path.join(directory_name, get_fixed_filename(name))
-            # os.rename(old_name, new_name)
+            os.rename(old_name, new_name)
             print(new_name)
+
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    word = []
+    for i, letter in enumerate(filename):
+        if letter.isupper():
+            word.append("_{}".format(letter))
+        else:
+            word.append(letter)
+    fixed = word[0].strip("_")
+    word = [fixed] + word[1:]
+    filename = "".join(word)
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt").replace("__", "_")
     return new_name
 
 
